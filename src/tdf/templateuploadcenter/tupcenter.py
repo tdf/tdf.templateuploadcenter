d5a6049953be9a7470a1fb0eca8e5691b0d9633d
andreasma <maand@gmx.de>
2015-08-11 20:00:11 +0200

    Added notifying for members about the availability of a new LibreOffice version in the versions list.

--- src/tdf/templateuploadcenter/tupcenter.py
+++ src/tdf/templateuploadcenter/tupcenter.py
@@ -207,6 +207,27 @@ def notifyAboutNewProject(tupproject, event):
 
 
 
+@grok.subscribe(ITUpCenter, IObjectModifiedEvent)
+def notifiyAboutNewVersion(tupproject, event):
+    if hasattr(event, 'descriptions') and event.descriptions:
+        for d in event.descriptions:
+            if d.interface is ITUpCenter and 'available_versions' in d.attributes:
+                users=api.user.get_users()
+                message='We added a new version of LibreOffice to the list.\n' \
+                        'Please add this version to your LibreOffice template release(s), ' \
+                        'if it is (they are) compatible with this version.\n\n' \
+                        'Kind regards,\n\n' \
+                        'The LibreOffice Extension and Template Site Administration Team'
+                for f in users:
+                    mailaddress = f.getProperty('email')
+                    api.portal.send_email(
+                        recipient=mailaddress,
+                        sender="noreply@libreoffice.org",
+                        subject="New Version of LibreOffice Added",
+                        body=message,
+                    )
+
+
 # Views
 
 class View(dexterity.DisplayForm):
