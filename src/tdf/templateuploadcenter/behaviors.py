446dbf4bffc646e011490255e1973b8619bde749
andreasma <maand@gmx.de>
2015-12-05 13:08:35 +0100

    Added a new behavior for creating a title of a release.

---
+++ src/tdf/templateuploadcenter/behaviors.py
@@ -0,0 +1,20 @@
+from zope.interface import implements
+from tdf.templateuploadcenter.interfaces import INameForRelease
+
+
+class NameForRelease(object):
+    """ Adapter to INameFromTitle
+    """
+
+    implements(INameForRelease)
+
+    def __init__(self, context):
+        self.context = context
+
+    def __new__(cls, context):
+        title = context.title
+        releasenumber = context.releasenumber
+        title = u'%s %s' % (title,releasenumber)
+        releasename = super(NameForRelease, cls).__new__(cls)
+        releasename.title = title
+        return releasename
