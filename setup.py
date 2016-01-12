e021a8659474d72ff492d37d84864fc20811fe1e
andreasma <maand@gmx.de>
2016-01-12 23:02:52 +0100

    Rework of some parts for Plone 5 compatibility and new structure.

--- setup.py
+++ setup.py
@@ -3,56 +3,45 @@ import os
 
 version = '0.1'
 
-long_description = (
-    open('README.txt').read()
-    + '\n' +
-    'Contributors\n'
-    '============\n'
-    + '\n' +
-    open('CONTRIBUTORS.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read()
-    + '\n')
-
 setup(name='tdf.templateuploadcenter',
       version=version,
       description="TDF Template Upload Center",
-      long_description=long_description,
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Environment :: Web Environment",
         "Framework :: Plone",
-        "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='Plone Add-On LibreOffice template templateuploadcenter',
       author='Andreas Mantke',
       author_email='maand@gmx.de',
       url='http://github.com/tdf/tdf.templateuploadcenter',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['tdf', ],
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['tdf'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
-          'Products.CMFPlone',
           'plone.app.dexterity',
+          'plone.namedfile [blobs]',
+          # -*- Extra requirements: -*-
           'collective.dexteritytextindexer',
           'cioppino.twothumbs',
           'plone.directives.form',
       ],
-      extras_require={'test': ['plone.app.testing[robot]>=4.2.2']},
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
+      # The next two lines may be deleted after you no longer need
+      # addcontent support from paster and before you distribute
+      # your package.
 #      setup_requires=["PasteScript"],
 #      paster_plugins=["templer.localcommands"],
+
       )
