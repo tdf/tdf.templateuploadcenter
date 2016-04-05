d318329a62bb1ce88b31b8b28bfa59594b58c1a8
andreasma <maand@gmx.de>
2016-04-05 21:31:11 +0200

    Replace HISTORY.txt with CHANGES.txt.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='tdf.templateuploadcenter',
       version=version,
       description="TDF Template Upload Center",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open(os.path.join("docs", "CHANGES.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
