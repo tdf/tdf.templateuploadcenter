17f345a9b2719b2ba0f3c30de3e416177a3c70c2
andreasma <maand@gmx.de>
2016-07-11 18:06:23 +0200

    PEP-8-Fix

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(name='tdf.templateuploadcenter',
       # The next two lines may be deleted after you no longer need
       # addcontent support from paster and before you distribute
       # your package.
-#      setup_requires=["PasteScript"],
-#      paster_plugins = ["ZopeSkel"],
+      # setup_requires=["PasteScript"],
+      # paster_plugins = ["ZopeSkel"],
 
       )
