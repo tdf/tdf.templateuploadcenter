84d0dc85bda65e3540478167b6e52b0aa4d4dff3
andreasma <maand@gmx.de>
2019-04-29 20:38:05 +0200

    Fix the info message.

--- tdf/templateuploadcenter/mailtoauthor.py
+++ tdf/templateuploadcenter/mailtoauthor.py
@@ -91,7 +91,8 @@ class MailToAuthorForm(AutoExtensibleForm, form.Form):
         # Redirect back to the front page with a status message
 
         IStatusMessage(self.request).addStatusMessage(
-                _(u"Thank you for your order. We will contact you shortly"),
+                _(u"We send your message to the author of the project. It's "
+                  u"on his choice, if he'll get back to you."),
                 "info"
             )
 
