642bba9742f2d6788f724f9cb88e54b4a1cba817
andreasma <maand@gmx.de>
2019-12-28 16:14:58 +0100

    Add missing pointer to an element of a list.

--- tdf/templateuploadcenter/tupreleaselink.py
+++ tdf/templateuploadcenter/tupreleaselink.py
@@ -100,7 +100,7 @@ def allowedtemplatefileextensions(context):
 def validatelinkedtemplatefileextension(value):
     catalog = api.portal.get_tool(name='portal_catalog')
     result = catalog.uniqueValuesFor('allowedtuctemplatefileextensions')
-    pattern = r'^.*\.({0})'.format(result)
+    pattern = r'^.*\.({0})'.format(result[0])
     matches = re.compile(pattern, re.IGNORECASE).match
     if not matches(value):
         raise Invalid(
